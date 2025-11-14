b5cf92f68e07c30ed8809d69b18a6005942b9432
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-11-14 16:28:17 -0600

    Fix long commits always auto-wrapping
    Check the user's setting before auto-wrapping. Wrap the message unless
    the user has set soft-wrap to none.

--- crates/git_ui/src/git_panel.rs
+++ crates/git_ui/src/git_panel.rs
@@ -39,6 +39,7 @@ use gpui::{
 };
 use itertools::Itertools;
 use language::{Buffer, File};
+use language::language_settings::{language_settings, SoftWrap as ConfigSoftWrap};
 use language_model::{
     ConfiguredModel, LanguageModel, LanguageModelRegistry, LanguageModelRequest,
     LanguageModelRequestMessage, Role,
@@ -1621,6 +1622,12 @@ impl GitPanel {
                 .filter(|message| !message.trim().is_empty());
         } else if message.trim().is_empty() {
             return None;
+        }
+        if let Some(language) = &git_commit_language {
+            let settings = language_settings(Some(language.name()), None, cx);
+            if settings.soft_wrap == ConfigSoftWrap::None {
+                return Some(message);
+            }
         }
         let buffer = cx.new(|cx| {
             let mut buffer = Buffer::local(message, cx);
